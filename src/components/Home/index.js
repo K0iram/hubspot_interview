5425b97cc70936427e280d0199c212ac5782053d
mario7746 <m.kennedykavouras@gmail.com>
2018-12-30 23:38:55 -0500

    code clean up and simplify functions

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,100 +1,95 @@
 import React, { Component } from 'react'
-import { fetchAvailability } from '../../api'
 import data from '../../api/sampleData'
 
 
 class Home extends Component {
   state = {
-    usa: [],
-    ireland: [],
-    mexico: [],
-    spain: [],
-    canada: [],
-    singapore: [],
-    japan: [],
-    uk: [],
-    france: []
+    partners: {},
+    bestDates: {},
+    loaded: false
   }
 
   componentDidMount() {
-    const usPartners = data.partners.filter(partner => partner.country === "United States")
-    const irePartners = data.partners.filter(partner => partner.country === "Ireland")
-    const mexPartners = data.partners.filter(partner => partner.country === "Mexico")
-    const spaPartners = data.partners.filter(partner => partner.country === "Spain")
-    const canPartners = data.partners.filter(partner => partner.country === "Canada")
-    const singPartners = data.partners.filter(partner => partner.country === "Singapore")
-    const japPartners = data.partners.filter(partner => partner.country === "Japan")
-    const ukPartners = data.partners.filter(partner => partner.country === "United Kingdom")
-    const fraPartners = data.partners.filter(partner => partner.country === "France")
+    let partners = {}
 
-    this.setState({
-      usa: usPartners,
-      mexico: mexPartners,
-      ireland: irePartners,
-      spain: spaPartners,
-      canada: canPartners,
-      singapore: singPartners,
-      japan: japPartners,
-      uk: ukPartners,
-      france: fraPartners,
-    })
+    data.partners
+      .forEach(partner => (
+        partners[partner.country]
+          ? partners[partner.country].push(partner)
+          : partners[partner.country] = [partner]
+      ))
+
+    this.setState({partners})
   }
 
-  pickBestDates = (partners, country) => {
-      let dates = {}
-      partners.map(partner => {
-        partner.availableDates.forEach(date => {
-          dates.hasOwnProperty(date) ? (
-            dates[date]++
-          ) : (
-            dates[date] = 1
-          )
-        })
+  pickBestDates = (entry) => {
+    const [country, partners] = entry
+    const dates = {}
+    let bestDates = {}
+
+    partners.forEach(partner => {
+      partner.availableDates.forEach(date => {
+        dates.hasOwnProperty(date) ? (
+          dates[date]++
+        ) : (
+          dates[date] = 1
+        )
       })
+    })
 
-      let datesArr = Object.entries(dates).sort((a,b) => a[0].split('-').join('') - b[0].split('-').join(''))
+    const datesArr = Object.entries(dates)
+      .sort((a,b) => (
+        a[0].split('-').join('') - b[0].split('-').join('')
+      ))
 
-      let bestDates = {}
+    for (let i = 0; i < datesArr.length; i++) {
+      if((datesArr[i+1] && datesArr[i][1] === datesArr[i+1][1])){
+        if(bestDates.hasOwnProperty(country) && datesArr[i][1] <= bestDates[country].attendeeCount) {
+        } else {
+          const people = this.state.partners[country].filter(person => person.availableDates.includes(datesArr[i][0]))
+          const attendees = people.map(p => p.email)
 
-      for (let i = 0; i < datesArr.length; i++) {
-        if((datesArr[i+1] && datesArr[i][1] === datesArr[i+1][1])){
-          if(bestDates.hasOwnProperty('startDate') && datesArr[i][1] <= bestDates.startDate.attendeeCount) {
-            bestDates = bestDates
-          } else {
-            let people = this.state[country].filter(person => person.availableDates.includes(datesArr[i][0]))
-            let attendees = people.map(p => p.email)
-
-            bestDates = {
-              'startDate': {
-                'date': datesArr[i][0],
-                'attendeeCount': datesArr[i][1],
-                'attendees': attendees,
-                'name': country
-              }
+          bestDates = {
+            [country]: {
+              'stateDate': datesArr[i][0],
+              'attendeeCount': datesArr[i][1],
+              'attendees': attendees,
+              'name': country
             }
           }
         }
       }
-      this.setState({[country]: Object.values(bestDates)})
     }
 
-    getAllDates = (countries) => {
-      countries.forEach(country => {
-        return this.pickBestDates(country[1], country[0])
-      })
-    }
+    this.setState((prevstate) => ({
+      bestDates: {
+        ...prevstate.bestDates,
+        ...bestDates
+      },
+      loaded: true
+    }))
+  }
+
+  getAllDates = () => {
+    Object.entries(this.state.partners).forEach(this.pickBestDates)
+  }
 
   render() {
-    const countriesArr = Object.entries(this.state)
+    const { loaded, bestDates } = this.state
     return (
       <div>
         <h1>Best Event Dates</h1>
-        <button onClick={() => this.getAllDates(countriesArr)} disabled={this.state.usa.length === 1}>Get Dates</button>
-        { this.state.usa.length === 1 &&
-          <div>
-            {JSON.stringify(this.state)}
-          </div>
-        }
+        <button onClick={this.getAllDates} disabled={loaded}>Get Dates</button>
+          {loaded &&
+
+            Object.values(bestDates).map((date, i) => (
+              <div key={i}>
+                <h4>{date.name}</h4>
+                <p>{date.stateDate}</p>
+                <p>Attendee Count: {date.attendeeCount}</p>
+              </div>
+            ))
+          }
       </div>
     )
   }
